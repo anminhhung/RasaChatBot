120a6d1b2fc76774d66d063df41a07f0eca26c6a
unknown <dauvannam321@gmail.com>
2024-05-25 21:50:48 +0700

    Update

---
+++ Test/test.py
@@ -0,0 +1,18 @@
+import asyncio
+from rasa.core.agent import Agent
+
+# # Load the trained model
+agent = Agent.load("Test\models")
+
+# # Parse a sample text
+# text = "Hello, how are you?"
+text = "hello world"
+result = asyncio.run(agent.parse_message(message_data=text))
+
+# # Print the features
+print(result)
+# print(result["text"])
+# print(result["intent"])
+# print(result["entities"])
+# print(result["text_features"])
+print("Done!")
