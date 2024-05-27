3bd73bf5fe00f07149728a04aed6bfa6edddd6f3
unknown <dauvannam321@gmail.com>
2024-05-27 17:23:58 +0700

    update

---
+++ SimpleRasaChatBot/TestAgent.py
@@ -0,0 +1,25 @@
+import asyncio
+from rasa.core.agent import Agent
+
+# # Load the trained model
+agent = Agent.load("RasaChatBot\models")
+
+# # Parse a sample text
+# text = "Hello, how are you?"
+text = "hello there, how are you"
+result = asyncio.run(agent.parse_message(message_data=text))
+
+# # # Print the features
+print(result)
+# # print(result["text"])
+# # print(result["intent"])
+# # print(result["entities"])
+# # print(result["text_features"])
+print("Done!")
+
+# from Custom.BARTPHOTOKENIZER import BartPhoTokenizer
+
+# text = "hello there"
+
+# tokens = BartPhoTokenizer.tokenize(text)
+# print(tokens)
